65fadc1ec543bad25952202a1070f1d8c2dcaf12
Jessie Ji <jji@verisk.com>
2019-09-19 06:39:46 -0600

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        Name: <input type="text"></input>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
