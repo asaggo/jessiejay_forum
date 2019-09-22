41bf710cf6dee42688c7246e5f51d3bfe5a3d117
Ji, Jessie <asaggo@byui.edu>
2019-09-22 16:09:37 -0600

    ant design button added

---
+++ client/src/components/Login.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Button from 'antd/es/button';
+
+import './Login.css';
+
+class Login extends Component{
+    render(){
+        return(
+            <div>
+                <Button type="primary">Button</Button>
+            </div>
+        );
+    }
+}
+
+export default Login;
